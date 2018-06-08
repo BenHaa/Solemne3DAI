1d66549bc34d7310df5bded766883ccdcfa1e62b
CETECOM <CETECOM@LC021014>
2018-06-08 14:17:20 -0300

    [DTO] :[Se crean las clases]

---
+++ PhpProyecto/dto/ComunaDto.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ComunaDto
+ *
+ * @author CETECOM
+ */
+class ComunaDto {
+    //put your code here
+}
